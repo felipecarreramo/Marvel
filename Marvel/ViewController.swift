5b7ed5842de004358e65d0b1f5d6d2391f91ae91
pipecamo <pipecamo@gmail.com>
2015-07-27 20:50:41 -0500

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Felipe Carrera on 27/07/15.
+//  Copyright (c) 2015 Juan Felipe Carrera Moya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
